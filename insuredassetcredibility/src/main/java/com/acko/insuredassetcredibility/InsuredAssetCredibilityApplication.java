53f357da004890fab40f41f6142e2a7b92cc07e7
Shivansh Sharma <117810471+shivanshsharma14@users.noreply.github.com>
2022-11-19 16:38:20 +0530

    Add files via upload

---
+++ insuredassetcredibility/src/main/java/com/acko/insuredassetcredibility/InsuredAssetCredibilityApplication.java
@@ -0,0 +1,13 @@
+package com.acko.insuredassetcredibility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuredAssetCredibilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuredAssetCredibilityApplication.class, args);
+	}
+
+}
---
+++ insuredassetcredibility/src/test/java/com/acko/insuredassetcredibility/InsuredAssetCredibilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acko.insuredassetcredibility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuredAssetCredibilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
