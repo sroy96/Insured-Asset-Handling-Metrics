351583a1fdecfb97f450e8da4f3012d3b71be8ae
saurav96-roy <saurav.roy@acko.tech>
2022-11-23 20:00:41 +0530

    Changes in Response DTO

--- src/main/java/com/acko/insuredassetcredibility/models/AssetScores.java
+++ src/main/java/com/acko/insuredassetcredibility/models/AssetScores.java
@@ -5,14 +5,11 @@
  */
 package com.acko.insuredassetcredibility.models;
 
-import com.acko.insuredassetcredibility.enums.Activities;
-import com.acko.insuredassetcredibility.enums.KeyFactors;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.*;
 
 import java.util.List;
-import java.util.Map;
 
 @Data
 @Builder
