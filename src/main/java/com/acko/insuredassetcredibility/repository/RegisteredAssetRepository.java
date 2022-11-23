d535de2e1d2f4240a6faa8202481003d0a3a9e82
saurav96-roy <saurav.roy@acko.tech>
2022-11-23 18:03:01 +0530

    Repository interface

---
+++ src/main/java/com/acko/insuredassetcredibility/repository/RegisteredAssetRepository.java
@@ -0,0 +1,17 @@
+/**
+ * @author saurav roy
+ * Date:21/11/22
+ * Time:12:37 AM
+ */
+package com.acko.insuredassetcredibility.repository;
+
+import com.acko.insuredassetcredibility.dao.RegisteredAssetDao;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RegisteredAssetRepository extends MongoRepository<RegisteredAssetDao,String> {
+    List<RegisteredAssetDao> findAllByOwnerMobileOrOwnerEmail(String mobile, String email);
+}
